463d0801c5000b2cd91e24924126c2f81f70b968
Vivek Parmar <58722335+V-Flash@users.noreply.github.com>
2023-10-26 14:45:55 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,19 @@
- function loading() {
-    var cacheBuster = new Date().getTime(); // Generate a cache-busting parameter based on the current time
-    var preloading = document.getElementById('preloading');
-    preloading.style.display = 'none';
-    preloading.style.background = 'url("../images/ice-cream.gif?' + cacheBuster + '")'; // Add the cache-busting parameter to the URL of the preloading image
-    document.documentElement.style.overflowY = 'auto';
-    document.body.style.overflowY = 'auto';
-  }
+$(document).ready(function() {
+   $(window).on("orientationchange", function(event) {
+       
+       var orientation = window.orientation;
 
+       if (orientation === 90 || orientation === -90) {
 
-  window.addEventListener("scroll", function () {
-  var columns = document.querySelectorAll(".image-container");
+           $(".title").addClass("landscape-title");
+           $(".tag").addClass("landscape-tag");
+           $(".tag-line").addClass("landscape-tag-line");
+       } 
 
-  columns.forEach(function (column) {
-    var images = column.querySelectorAll(".whatwedo-img");
-
-    images.forEach(function (image) {
-      var imagePosition = image.getBoundingClientRect().top;
-      var screenPosition = window.innerHeight / 1.2;
-
-      if (imagePosition < screenPosition) {
-        image.classList.add("custom-animation");
-      } 
-      else {
-        image.classList.remove("custom-animation");
-      }
-
-    });
-
-  });
-
+       else {
+           $(".title").removeClass("landscape-title");
+           $(".tag").removeClass("landscape-tag");
+           $(".tag-line").removeClass("landscape-tag-line");
+       }
+   });
 });
